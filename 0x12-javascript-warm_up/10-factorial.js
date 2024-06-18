fc00f2ad7321ab880be63f0b5db81c8aeb1da4ec
root <ikrambouachra2@gmail.com>
2024-06-18 06:20:30 -0700

    js-warm-up

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+function factorial(n) {
+  // Base case: factorial of 0 is 1
+  if (isNaN(n) || parseInt(n, 10) !== n) {
+    return 1;
+  } else if (n === 0) {
+    return 1;
+  } else {
+    // Recursive case: n * factorial(n-1)
+    return n * factorial(n - 1);
+  }
+}
+
+const args = process.argv.slice(2);
+const num = parseInt(args[0], 10);
+
+console.log(factorial(num));
+

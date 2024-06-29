062fd533fb8ceef8ea9011b3c5e15e3bb49e0764
Akame <116276191+Akame0xg1rl@users.noreply.github.com>
2024-06-29 03:15:51 +0100

    Update 0-select_states.py fix the connection

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,43 +1,16 @@
 #!/usr/bin/python3
-import sys
+"""Lists states"""
+
 import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database".format(sys.argv[0]))
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    try:
-        # Connect to MySQL database
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database
-        )
-
-        # Create a cursor object using cursor() method
-        cursor = db.cursor()
-
-        # Execute SQL query to fetch all states sorted by id
-        cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-        # Fetch all the rows in a list of tuples
-        results = cursor.fetchall()
-
-        # Print each state tuple in the format (id, name)
-        for row in results:
-            print(row)
-
-        # Disconnect from server
-        db.close()
-
-    except MySQLdb.Error as e:
-        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
-        sys.exit(1)
-
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()

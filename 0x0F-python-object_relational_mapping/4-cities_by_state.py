299bfeee6f45dbd63089deaa6dbfcc3ef95e9707
Akame <116276191+Akame0xg1rl@users.noreply.github.com>
2024-06-29 03:23:36 +0100

    Update 4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,51 +1,18 @@
 #!/usr/bin/python3
-import sys
+"""Lists states"""
+
 import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database".format(sys.argv[0]))
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    try:
-        # Connect to MySQL database
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database
-        )
-
-        # Create a cursor object using cursor() method
-        cursor = db.cursor()
-
-        # Prepare SQL query to fetch cities with corresponding state names
-        sql_query = """
-                    SELECT cities.id, cities.name, states.name
-                    FROM cities
-                    JOIN states ON cities.state_id = states.id
-                    ORDER BY cities.id ASC
-                    """
-
-        # Execute the SQL query
-        cursor.execute(sql_query)
-
-        # Fetch all the rows in a list of tuples
-        results = cursor.fetchall()
-
-        # Print each city tuple in the format (id, city_name, state_name)
-        for row in results:
-            print(row)
-
-        # Disconnect from server
-        db.close()
-
-    except MySQLdb.Error as e:
-        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
-        sys.exit(1)
-
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities "
+                "JOIN states ON cities.state_id = states.id "
+                "ORDER BY cities.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()

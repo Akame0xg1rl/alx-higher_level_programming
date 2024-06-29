a7a88c1ce6a9b36fcc05c953d473cf51736d5c5c
Akame <116276191+Akame0xg1rl@users.noreply.github.com>
2024-06-29 03:00:01 +0000

    gho

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,36 +1,29 @@
 #!/usr/bin/python3
-"""Prints the State object with the name passed as argument from the database hbtn_0e_6_usa"""
-
+"""
+Lists the State object with the name passed as argument
+from the database hbtn_0e_6_usa.
+Usage: ./10-model_state_my_get.py <mysql username> /
+                                   <mysql password> /
+                                   <database name>
+                                   <state name searched>
+"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from model_state import State
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: {} username password database state_name".format(sys.argv[0]))
-        sys.exit(1)
-
-    username, password, database, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
-
-    # Create an engine to interact with the database
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(username, password, database))
-
-    # Create a configured "Session" class
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
-
-    # Create a Session instance
     session = Session()
 
-    # Query the State object by name
-    state = session.query(State).filter(State.name == state_name).first()
-
-    # Print the result or 'Not found'
-    if state:
-        print(state.id)
-    else:
+    found = False
+    for state in session.query(State):
+        if state.name == sys.argv[4]:
+            print("{}".format(state.id))
+            found = True
+            break
+    if found is False:
         print("Not found")
-
-    # Close the session
-    session.close()
-
--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,36 +1,24 @@
 #!/usr/bin/python3
-"""Prints the first State object from the database hbtn_0e_6_usa"""
-
+""""
+Prints the first State object from the database hbtn_0e_6_usa.
+Usage: ./8-model_state_fetch_first.py <mysql username> /
+                                       <mysql password> /
+                                       <database name>
+"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from model_state import State
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database".format(sys.argv[0]))
-        sys.exit(1)
-
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-
-    # Create an engine to interact with the database
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(username, password, database))
-
-    # Create a configured "Session" class
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
-
-    # Create a Session instance
     session = Session()
 
-    # Query the first State object
-    first_state = session.query(State).order_by(State.id).first()
-
-    # Print the result in the specified format or 'Nothing' if no state found
-    if first_state:
-        print("{}: {}".format(first_state.id, first_state.name))
-    else:
+    state = session.query(State).order_by(State.id).first()
+    if state is None:
         print("Nothing")
-
-    # Close the session
-    session.close()
-
+    else:
+        print("{}: {}".format(state.id, state.name))

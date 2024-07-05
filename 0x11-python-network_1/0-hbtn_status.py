182b4646b53c42f6b1c9e4b61fa7283bcedb92c9
Akame0xg1rl <ikrambouachra2@gmail.com>
2024-07-05 04:06:46 -0700

    status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
+"""
+This module fetches the URL https://alx-intranet.hbtn.io/status
+and displays the body of the response with specific formatting.
+"""
+
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
 
-with urllib.request.urlopen(url) as response:
-    content = response.read()
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
 
-print("Body response:")
-print(f"\t- type: {type(content)}")
-print(f"\t- content: {content}")
-print(f"\t- utf8 content: {content.decode('utf-8')}")
+    print("Body response:")
+    print(f"\t- type: {type(content)}")
+    print(f"\t- content: {content}")
+    print(f"\t- utf8 content: {content.decode('utf-8')}")
 
